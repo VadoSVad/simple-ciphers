a5063a8b983e9652f0289d7d526bd12227e105c5
VadoSVad <vadimsol@yandex.ru>
2024-04-13 01:29:15 +0300

    Add a bin-encrypt and bin-decrypt

--- simple-ciphers.js
+++ simple-ciphers.js
@@ -1,15 +1,18 @@
 const SimpleCiphers = {
 
-    val_encrypt: (text, val) => {
-        const letters = text.split('');
-
-        let enc_text = '';
-
-        for (letter in letters){
-            enc_text += letter.charCodeAt(0).toString(val);
-        }
+    bin_encrypt: (text) => {
+        let enc_text = text.split('').map(char => {
+            return char.charCodeAt(0).toString(2).padStart(8, '0')
+        }).join('')
         return enc_text;
     },
+
+    bin_decrypt: (enc_text) => {
+        let dec_text = enc_text.match(/.{1,8}/g).map(bin => {
+            return String.fromCharCode(parseInt(bin, 2))
+        }).join('')
+        return dec_text;
+    }
 
 };
 
