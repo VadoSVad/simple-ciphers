bd68da622ac2baede78477b951a344c38e9c4089
VadoSVad <vadimsol@yandex.ru>
2024-04-13 18:22:09 +0300

    Added Morse code-cipher

--- simple-ciphers.js
+++ simple-ciphers.js
@@ -22,13 +22,39 @@ const SimpleCiphers = {
         return caesar_text;
     },
 
-    caesar_decrypt: (enc_text, step) => {
+    caesar_decrypt: (caesar_text, step) => {
         let dec_text = '';
-        for (t in enc_text) {
-            dec_text += String.fromCharCode(enc_text.charCodeAt(t) - step);
+        for (t in caesar_text) {
+            dec_text += String.fromCharCode(caesar_text.charCodeAt(t) - step);
         }
         return dec_text;
-    }
+    },
+
+    morseAlphabet: {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+        'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+        'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+        'Y': '-.--', 'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+        '7': '--...', '8': '---..', '9': '----.',
+        '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+        ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-',
+        '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+    },
+
+    morse_encrypt: (text) => {
+        let morse = text.toUpperCase().split('').map(t => {
+            return SimpleCiphers.morseAlphabet[t];
+        }).join(' ')
+        return morse;
+    },
+
+    morse_decrypt: (morse) => {
+        let dec_text = morse.split(' ').map(t => {
+            return Object.keys(SimpleCiphers.morseAlphabet).find(key => SimpleCiphers.morseAlphabet[key] === t)
+            }).join('');
+        return dec_text;
+    },
 
 };
 
